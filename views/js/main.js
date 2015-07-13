188343bdd83c69eaaf50579b50a61e0b5ed5922f
Jeff Hoffman <jkhoffman@gmail.com>
2015-07-13 02:28:19 -0400

    Optimize pizza site.

--- views/js/main.js
+++ views/js/main.js
@@ -497,16 +497,20 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // The following code for sliding background pizzas was pulled from Ilya's demo found at:
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
+var items;
+
 // Moves the sliding background pizzas based on scroll position
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    requestAnimationFrame((function (i, phase) {
+      return function () {
+        items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+      }
+    })(i, phase));
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
@@ -536,5 +540,6 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
+  items = document.querySelectorAll('.mover');
   updatePositions();
 });
